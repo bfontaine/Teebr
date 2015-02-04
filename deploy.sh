f08f5a3b42afa2579b713e8d468e3393d87d1c52
Baptiste Fontaine <batifon@yahoo.fr>
2015-02-04 11:18:04 +0100

    dokku run workaround

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@
 git checkout production
 git rebase master
 git push -f prod production:master
-ssh root@bftapp dokku run teebr.co make "PYBABEL=pybabel" babel-compile
-git checkout @{-1}
+echo "run 'make PYBABEL=pybabel babel-compile' on the app now"
+#ssh root@bftapp dokku run teebr.co make "PYBABEL=pybabel" babel-compile
+git checkout master
