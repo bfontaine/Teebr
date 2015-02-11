3b9c048baa697b413884fc9ab91974421feca491
Baptiste Fontaine <batifon@yahoo.fr>
2015-02-11 17:06:29 +0100

    Emojis detection fixed

--- teebr/textutils.py
+++ teebr/textutils.py
@@ -1,7 +1,5 @@
 # -*- coding: UTF-8 -*-
 
-from __future__ import unicode_literals
-
 import re
 
 # codes from http://stackoverflow.com/a/12824140/735926
@@ -9,10 +7,9 @@ RE_EMOJI = re.compile(
         "["
             r"\U00002600-\U000026FF"  # Misc Symbols
             r"\U00002700-\U000027BF"  # Dingbats
-        #   TODO:
-        #   r"\U0001F300-\U0001F5FF"  # Misc Symbols & Pictographs
-        #   r"\U0001F600-\U0001F64F"  # Emoticons
-        #   r"\U0001F680-\U0001F6FF"  # Transport & Map Symbols
+            r"\U0001F300-\U0001F5FF"  # Misc Symbols & Pictographs
+            r"\U0001F600-\U0001F64F"  # Emoticons
+            r"\U0001F680-\U0001F6FF"  # Transport & Map Symbols
         "]"
 , re.UNICODE)
 
