440fb134115ff2732d7d61bc0d29318d48e0ea65
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-11 13:00:44 +0100

    datasets.py: get all tweets by default

--- datasets.py
+++ datasets.py
@@ -14,11 +14,11 @@ def list():
     return [s[_prefix_len:-_suffix_len] \
             for s in glob("%s*%s" % (_prefix, _suffix))]
 
-def get(name, mx=10000):
+def get(name, mx=-1):
     ss = []
-    with open("datasets/statuses-%s.jsons" % name) as f:
+    with open("%s%s%s" % (_prefix, name, _suffix)) as f:
         for i, l in enumerate(f):
-            if i > mx:
+            if mx > 0 and i > mx:
                 break
             ss.append(Status.parse(None, loads(l)))
     return ss
